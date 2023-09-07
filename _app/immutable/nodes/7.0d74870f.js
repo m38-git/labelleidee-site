daa13fef19acc2c080c75f825d8a4db7302252ce
roland <achatrm@free.fr>
2023-09-07 09:47:04 +0200

    premier commit

---
+++ _app/immutable/nodes/7.0d74870f.js
@@ -0,0 +1,15 @@
+import{d as o}from"../chunks/environment.9aa685ef.js";import{s as l,q as r}from"../chunks/scheduler.ba9b7ec6.js";import{S as i,i as c,g as u,h as b,J as d,k as m,a as p,f as h}from"../chunks/index.14e2b25e.js";const f=o,x=!0,S=Object.freeze(Object.defineProperty({__proto__:null,csr:f,prerender:x},Symbol.toStringTag,{value:"Module"}));function g(a){let e,s=`<h1 class="pt-10 text-7xl">Mentions légales</h1> <h2 class="pt-10 text-4xl">éditeur du site internet :</h2>
+La Belle Idée<br/> <br/>
+7, Cours Jean Jaurès<br/> <br/>
+38000 Grenoble<br/> <br/>
+Tel. 04.76.47.52.95<br/> <br/>
+Capital social 5.000,00 €<br/> <br/>
+RCS Grenoble B 521 357 665<br/> <h2 class="pt-10 text-4xl">Responsable éditorial du site internet :</h2>
+directeur de la publication, responsable de la rédaction: Guillaume MARC 
+<h2 class="pt-10 text-4xl">Hébergeur du site internet :</h2>
+choix en cours
+<h2 class="pt-10 text-4xl">Fournisseur du nom de domaine :</h2>
+Infomaniak Network SA<br/> <br/>
+Rue Eugène Marziano 25<br/> <br/>
+1227 Les Acacias (GE)<br/> <br/>
+Plus d&#39;informations sur https://www.infomaniak.com/fr/cgv/mentions-legales<br/> <br/>`;return{c(){e=u("section"),e.innerHTML=s,this.h()},l(t){e=b(t,"SECTION",{class:!0,"data-svelte-h":!0}),d(e)!=="svelte-1qszf3c"&&(e.innerHTML=s),this.h()},h(){m(e,"class","text-center w-full bg-gradient-to-r from-amber-600 via-red-700 to-amber-800")},m(t,n){p(t,e,n)},p:r,i:r,o:r,d(t){t&&h(e)}}}class M extends i{constructor(e){super(),c(this,e,null,g,l,{})}}export{M as component,S as universal};
